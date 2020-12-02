f4b48837a2f7ea69d3843cde1db4c2ca272e0892
Amirhossein <amir.h.nouri2000@gmail.com>
2020-12-02 19:49:08 +0330

    full screen maze added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
 const cells = 3;
-const width = 600;
-const height = 600;
+const width = window.innerWidth;
+const height = window.innerHeight;
 
 const unitLength = width / cells;
 
