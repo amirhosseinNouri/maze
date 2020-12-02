90702bbaf0b45614921c9e6b8d38e3aa1c56f1d3
Amirhossein <amir.h.nouri2000@gmail.com>
2020-12-02 18:43:15 +0330

    generate maze function, completed

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const {
   Mouse,
 } = Matter;
 
+const cells = 3;
 const width = 800;
 const height = 600;
 
@@ -19,7 +20,7 @@ const render = Render.create({
   options: {
     width,
     height,
-    wireframes : false
+    wireframes: false,
   },
 });
 
@@ -43,23 +44,83 @@ const walls = [
 
 World.add(world, walls);
 
-// Random Shapes
+// Maze generation
 
-for (let i = 0; i < 50; i++) {
-    if(Math.random() > 0.5){
-        World.add(
-            world,
-            Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
-          );
-    }else{
-        World.add(
-            world,
-            Bodies.circle(Math.random() * width, Math.random() * height, 35 , {
-                render: {
-                    fillStyle : 'chocolate'
-                }
-            })
-          );
+const shuffle = (arr) => {
+  let counter = arr.length;
+
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
+
+    counter--;
+
+    const temp = arr[counter];
+    arr[counter] = arr[index];
+    arr[index] = temp;
+  }
+
+  return arr;
+};
+
+const grid = Array(cells)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+const verticals = Array(cells)
+  .fill(null)
+  .map(() => Array(cells - 1).fill(false));
+const horizontals = Array(cells - 1)
+  .fill(null)
+  .map(() => Array(cells - 1).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const recurse = (row, column) => {
+  // If i have visited the cell at [row,column], then return.
+  if (grid[row][column]) return;
+
+  // Mark this cell as visited.
+  grid[row][column] = true;
+
+  // Order neighbors randomly.
+  const neighbors = shuffle([
+    [row - 1, column , 'up'],
+    [row, column + 1 , 'right'],
+    [row + 1, column , 'down'],
+    [row, column - 1 , 'left'],
+  ]);
+
+  for (neighbor of neighbors) {
+    const [nextRow, nextColumn , direction] = neighbor;
+    // See if that neighbor is out of bound.
+    if (
+      nextRow < 0 ||
+      nextRow >= cells ||
+      nextColumn < 0 ||
+      nextColumn >= cells
+    ) {
+      continue;
     }
- 
-}
+
+    // See if we visited this neighor, continue to the next neighbor
+    if (grid[nextRow][nextColumn]) continue;
+
+    // Remove a wall from either horizontals or verticals
+    if(direction == 'left'){
+        verticals[row][column-1] = true
+    }
+    else if(direction === 'right'){
+        verticals[row][column] = true
+    }
+    else if(direction === 'up'){
+        horizontals[row-1][column] = true
+    }else if(direction === 'down') {
+        horizontals[row][column] = true
+    }
+
+    // Move to the next cell
+    recurse(nextRow , nextColumn)
+  }
+};
+
+recurse(startRow, startColumn);
