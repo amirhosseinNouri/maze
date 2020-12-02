b087d2b891f9d95c104b449f112ebdef108bc781
Amirhossein <amir.h.nouri2000@gmail.com>
2020-12-02 19:12:42 +0330

    maze wall generating done

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-const {
-  Engine,
-  Render,
-  Runner,
-  World,
-  Bodies,
-  MouseConstraint,
-  Mouse,
-} = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
 const cells = 3;
-const width = 800;
+const width = 600;
 const height = 600;
+
+const unitLength = width / cells;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -18,30 +12,21 @@ const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
+    wireframes: true,
     width,
     height,
-    wireframes: false,
   },
 });
-
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(
-  world,
-  MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas),
-  })
-);
-
 // Walls
 const walls = [
-  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
-  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
-  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
-  Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }),
 ];
-
 World.add(world, walls);
 
 // Maze generation
@@ -65,34 +50,39 @@ const shuffle = (arr) => {
 const grid = Array(cells)
   .fill(null)
   .map(() => Array(cells).fill(false));
+
 const verticals = Array(cells)
   .fill(null)
   .map(() => Array(cells - 1).fill(false));
+
 const horizontals = Array(cells - 1)
   .fill(null)
-  .map(() => Array(cells - 1).fill(false));
+  .map(() => Array(cells).fill(false));
 
 const startRow = Math.floor(Math.random() * cells);
 const startColumn = Math.floor(Math.random() * cells);
 
-const recurse = (row, column) => {
-  // If i have visited the cell at [row,column], then return.
-  if (grid[row][column]) return;
+const stepThroughCell = (row, column) => {
+  // If i have visted the cell at [row, column], then return
+  if (grid[row][column]) {
+    return;
+  }
 
-  // Mark this cell as visited.
+  // Mark this cell as being visited
   grid[row][column] = true;
 
-  // Order neighbors randomly.
+  // Assemble randomly-ordered list of neighbors
   const neighbors = shuffle([
-    [row - 1, column , 'up'],
-    [row, column + 1 , 'right'],
-    [row + 1, column , 'down'],
-    [row, column - 1 , 'left'],
+    [row - 1, column, "up"],
+    [row, column + 1, "right"],
+    [row + 1, column, "down"],
+    [row, column - 1, "left"],
   ]);
+  // For each neighbor....
+  for (let neighbor of neighbors) {
+    const [nextRow, nextColumn, direction] = neighbor;
 
-  for (neighbor of neighbors) {
-    const [nextRow, nextColumn , direction] = neighbor;
-    // See if that neighbor is out of bound.
+    // See if that neighbor is out of bounds
     if (
       nextRow < 0 ||
       nextRow >= cells ||
@@ -102,25 +92,61 @@ const recurse = (row, column) => {
       continue;
     }
 
-    // See if we visited this neighor, continue to the next neighbor
-    if (grid[nextRow][nextColumn]) continue;
+    // If we have visited that neighbor, continue to next neighbor
+    if (grid[nextRow][nextColumn]) {
+      continue;
+    }
 
     // Remove a wall from either horizontals or verticals
-    if(direction == 'left'){
-        verticals[row][column-1] = true
-    }
-    else if(direction === 'right'){
-        verticals[row][column] = true
-    }
-    else if(direction === 'up'){
-        horizontals[row-1][column] = true
-    }else if(direction === 'down') {
-        horizontals[row][column] = true
+    if (direction === "left") {
+      verticals[row][column - 1] = true;
+    } else if (direction === "right") {
+      verticals[row][column] = true;
+    } else if (direction === "up") {
+      horizontals[row - 1][column] = true;
+    } else if (direction === "down") {
+      horizontals[row][column] = true;
     }
 
-    // Move to the next cell
-    recurse(nextRow , nextColumn)
+    stepThroughCell(nextRow, nextColumn);
   }
 };
 
-recurse(startRow, startColumn);
+stepThroughCell(startRow, startColumn);
+
+horizontals.forEach((row, rowIndex) => {
+  row.forEach((open, columnIndex) => {
+    if (open) {
+      return;
+    }
+
+    const wall = Bodies.rectangle(
+      columnIndex * unitLength + unitLength / 2,
+      rowIndex * unitLength + unitLength,
+      unitLength,
+      10,
+      {
+        isStatic: true,
+      }
+    );
+    World.add(world, wall);
+  });
+});
+
+verticals.forEach((row, rowIndex) => {
+  row.forEach((open, columnIndex) => {
+    if (open) return;
+
+    const wall = Bodies.rectangle(
+      columnIndex * unitLength + unitLength,
+      rowIndex * unitLength + unitLength / 2,
+      10,
+      unitLength,
+      {
+        isStatic: true,
+      }
+    );
+
+    World.add(world, wall);
+  });
+});
