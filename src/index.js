e07047452d05d5e3b420f7277fe0459f4b7bc952
Amirhossein <amir.h.nouri2000@gmail.com>
2020-12-02 19:40:13 +0330

    ball and goal collision add

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { Engine, Render, Runner, World, Bodies, Body } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
 const cells = 3;
 const width = 600;
@@ -161,13 +161,16 @@ const goal = Bodies.rectangle(
   unitLength * 0.7,
   {
     isStatic: true,
+    label : 'goal'
   }
 );
 World.add(world, goal);
 
 // Ball
 
-const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4);
+const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4, {
+    label : 'ball'
+});
 
 World.add(world, ball);
 
@@ -175,8 +178,6 @@ document.addEventListener("keydown", (event) => {
 
 
     const {x,y} = ball.velocity
-    console.log(x);
-    console.log(y);
 
   // Up
   if (event.keyCode === 87) {
@@ -195,3 +196,16 @@ document.addEventListener("keydown", (event) => {
     Body.setVelocity(ball, {x: x - 5 , y})
   }
 });
+
+
+// Win condition
+
+Events.on(engine , 'collisionStart' , event =>{
+    event.pairs.forEach((collision) =>{
+        const labels = ['ball' , 'goal']
+
+        if(labels.includes(collision.bodyA.label) &&labels.includes(collision.bodyB.label) ){
+            console.log('user won');
+        }
+    })
+})
